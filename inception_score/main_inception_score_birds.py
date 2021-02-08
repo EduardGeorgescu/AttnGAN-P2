0fe1993e17a010a78ff6618e6130cf47a0856688
EDY <eduard.georgescu@yahoo.com>
2021-02-08 13:37:29 +0200

    inception score coco + 1 capt per img

---
+++ inception_score/main_inception_score_birds.py
@@ -0,0 +1,51 @@
+# Folosit inception score de la:
+# https://github.com/openai/improved-gan
+
+from model import get_inception_score
+import os
+import cv2
+
+IMAGE_DIR = "/content/DATASET_DRIVE/MyDrive/validation/valid/single"
+# "/home/edy/Desktop/Master/Semestrul 2/Research/AttnGAN-Python2/models/bird_AttnGAN2/valid/single/"
+
+def main():
+    # print ("suntem in main!!!")
+
+    big_dir = os.listdir(IMAGE_DIR)
+    big_dir = sorted(big_dir)
+    list_images = []
+
+    print ("IMAGE_DIR is" + IMAGE_DIR)
+
+    index_dir = 0
+    for img_category in big_dir:
+        dir_path = IMAGE_DIR + "/" + img_category
+        # print (dir_path)
+
+        list_img_paths = os.listdir(dir_path)
+
+        cnt = 0
+        for img_path in list_img_paths:
+            # print (img_path)
+            cnt += 1
+
+            crt_path = dir_path + "/" + img_path
+            # print (crt_path)
+            crt_img = cv2.imread(crt_path)
+
+            # print (type(crt_img))
+            list_images.append(crt_img)
+
+        index_dir += 1
+        print (str(index_dir) + ". Total of " + str(cnt) + " images at " + img_category)
+
+    # print (list_img_paths)
+    print ("We have a total of: " + str(len(list_images)) + " images!")
+    nr_splits = 10
+    results = get_inception_score(list_images, splits=nr_splits)
+
+    print ("Results are:", results)
+    print ("Nr_splits were: " + str(nr_splits))
+
+if __name__ == "__main__":
+    main()
--- inception_score/main_inception_score_coco.py
+++ inception_score/main_inception_score_coco.py
@@ -5,7 +5,7 @@ from model import get_inception_score
 import os
 import cv2
 
-IMAGE_DIR = "/content/BIRDS/My Drive/validation/valid/single"
+IMAGE_DIR = "/content/DATASET_DRIVE/MyDrive/validation/valid/single"
 # "/home/edy/Desktop/Master/Semestrul 2/Research/AttnGAN-Python2/models/bird_AttnGAN2/valid/single/"
 
 def main():
@@ -17,27 +17,21 @@ def main():
 
     print ("IMAGE_DIR is" + IMAGE_DIR)
 
-    index_dir = 0
-    for img_category in big_dir:
-        dir_path = IMAGE_DIR + "/" + img_category
-        # print (dir_path)
+    list_img_paths = os.listdir(big_dir)
 
-        list_img_paths = os.listdir(dir_path)
+    cnt = 0
+    for img_path in list_img_paths:
+        # print (img_path)
+        cnt += 1
 
-        cnt = 0
-        for img_path in list_img_paths:
-            # print (img_path)
-            cnt += 1
+        crt_path = dir_path + "/" + img_path
+        # print (crt_path)
+        crt_img = cv2.imread(crt_path)
 
-            crt_path = dir_path + "/" + img_path
-            # print (crt_path)
-            crt_img = cv2.imread(crt_path)
-
-            # print (type(crt_img))
-            list_images.append(crt_img)
+        # print (type(crt_img))
+        list_images.append(crt_img)
 
         index_dir += 1
-        print (str(index_dir) + ". Total of " + str(cnt) + " images at " + img_category)
 
     # print (list_img_paths)
     print ("We have a total of: " + str(len(list_images)) + " images!")
